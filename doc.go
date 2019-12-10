a2b720d7669fb95c09ea53358cf4eb5f650cdee7
nanmu42 <i@nanmu.me>
2019-12-10 18:10:43 +0800

    doc: linebreak

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
 // Package gzip implements gzip middleware for Gin and net/http.
 //
 // * introduction: https://github.com/nanmu42/gzip
+//
 // * examples: https://github.com/nanmu42/gzip/tree/master/examples
 package gzip
