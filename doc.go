1a043c2702f812c8258b9b429eaaf4b017949b99
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-24 21:24:48 +0800

    Delete doc.go

--- doc.go
+++
@@ -1,6 +0,0 @@
-// Package gzip implements gzip middleware for Gin and net/http.
-//
-// * introduction: https://github.com/nanmu42/gzip
-//
-// * examples: https://github.com/nanmu42/gzip/tree/master/examples
-package gzip
